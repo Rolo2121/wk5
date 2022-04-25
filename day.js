dceed10443b5b39b1559f4d9a89680642c0b1cef
Jacob Bauer <100535724+Rolo2121@users.noreply.github.com>
2022-04-24 22:14:01 -0500

    Add files via upload

---
+++ day.js
@@ -0,0 +1,11 @@
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+})
